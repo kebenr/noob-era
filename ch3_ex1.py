bfc9ec2544c5b8895ec526abbb44a428ff4936e8
kebenr <kebenromana@gmail.com>
2022-01-17 10:09:36 -0800

    Update ch3_ex1.py

--- ch3_ex1.py
+++ ch3_ex1.py
@@ -1,27 +1,28 @@
-hours = input('enter hours ')
-rate = input('enter rate ')
+hours = 45
+rate = 10
 pay = 'dog'
 
 try:
     rate_check = int(rate)
 except:
     rate_check = -1
-#overtime pay
-if rate_check > 0 :
-    if hours > 40:
-        rate = rate * 1.5
-
-else:
+if rate_check == -1:
     print('error, please input numeric value')
 
 try:
     hours_check = int(hours)
 except:
     hours_check = -1
-
 if hours_check == -1:
     print('error, please enter number value')
-if hours_check > 0:
-    pay = (hours - 40) * rate + 400
+
+if hours_check and rate_check > 0:
+    if hours <= 40:
+        pay = hours * rate
+    elif hours > 40:
+        basepay = 40 * rate
+        rate = rate * 1.5
+        pay = (hours - 40) * rate + basepay
+
 
 print('Pay: $' + str(pay))
