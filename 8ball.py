400587f455129974f23cd6aee6a2ba0733a4b3f9
kebenr <kebenromana@gmail.com>
2022-01-19 07:04:33 -0800

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -1,30 +1,11 @@
 import random
 name = input('put name here: ')
 question = input('whats your question dog? ')
-answer = ['yes dog', 'decidely brother', 'doubt none', 'faded. try again', 'im lazy rn ask later',
+answer = ['yes dog', 'decidely brother', 'no doubt esketit', 'faded. try again', 'im lazy rn ask later',
 'better off not knowing rn', 'lmao nah', 'not so good', 'very sus']
 
-random_number = random.randint(1,9)
-if random_number == 1:
-  answer = answer[0]
-elif random_number == 2:
-  answer = answer[1]
-elif random_number == 3:
-  answer = answer[2]
-elif random_number == 4:
-  answer = answer[3]
-elif random_number == 5:
-  answer = answer[4]
-elif random_number == 6:
-  answer = answer[5]
-elif random_number == 7:
-  answer = answer[6]
-elif random_number == 8:
-  answer = answer[7]
-elif random_number == 9:
-  answer = answer[8]
-else:
-  answer = ('Error')
+dog = random.randint(0,8)
+answer = answer[dog]
 
 if question == (''):
   print ('yo dawg you fucced up lemme get a question')
@@ -36,3 +17,4 @@ else:
   print ("Magic 8-Ball's answer: " + answer)
 
 #changelog replaced individual strings under each elif with a list, added user input
+#condense if elif statements
